e0528487a8a3ca02ed5daa56cf1b6b712e4e0f3b
Spurch <ivan.stoyanov.nikolov@gmail.com>
2015-02-28 22:02:51 +0200

    C# Part2 - 2013/2014 @ 14 Sept 2013 - Morning Task#1 - Magic Words
    A pretty straightforward solution of the problem.

---
+++ MagicWords_solution01.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+class Program
+{
+    static void Main()
+    {
+        int Value = Int32.Parse(Console.ReadLine());
+        List<string> StrArray = new List<string>();
+        for (int i = 0; i < Value; i++)
+        {
+            StrArray.Add(Console.ReadLine());
+        }
+        for (int i = 0; i < Value; i++)
+        {
+            string CurrentString = StrArray[i];
+            int Index = CurrentString.Length % (Value + 1);
+            StrArray.Insert(Index, CurrentString);
+            if (Index < i)
+            {
+                StrArray.RemoveAt(i + 1);
+            }
+            else
+            {
+                StrArray.RemoveAt(i);
+            }
+            
+        }
+        StringBuilder Result = new StringBuilder();
+        int MaxLength = 0;
+        foreach (var Str in StrArray)
+        {
+            MaxLength = Math.Max(MaxLength, Str.Length);
+        }
+        for (int i = 0; i < MaxLength; i++)
+        {
+            foreach (var Str in StrArray)
+            {
+                if (Str.Length > i)
+                {
+                    Result.Append(Str[i]);
+                }
+            }
+        }
+        Console.WriteLine(Result.ToString());
+    }
+}
+
