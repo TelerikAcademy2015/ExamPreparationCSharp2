6ca73542a8ee2e0a9a096e3d4a9fa84d15197ed7
Antoan Elenkov <antoanelenkov@abv.bg>
2015-03-03 00:44:50 +0200

    +bunnyFactory

--- 2014.Вариант 1 - 22 януари/03.BunnyFactory/Bunny.cs
+++ 2014.Вариант 1 - 22 януари/03.BunnyFactory/Bunny.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Numerics;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -9,7 +10,69 @@ class Program
 {
     static void Main()
     {
+        var list = new List<int>();
+        string cage = string.Empty;
 
+        while (true)
+        {
+            cage = Console.ReadLine();
+            if (cage != "END")
+            {
+                list.Add(int.Parse(cage));
+            }
+            else { break; }
+        }
+        int cycleCounter = 1;
+
+        while (true)
+        {
+            //checks for end of the game
+            if (list.Count < cycleCounter)
+            {
+                break;
+            }
+
+            //find the sum of the first i-th elements
+            int sum = 0;
+            for (int i = 0; i < cycleCounter; i++)
+            {
+                sum += list[i];
+            }
+            
+            //checks if there are "s" cages
+            if (sum + cycleCounter > list.Count)
+            {
+                break;
+            }
+
+            //find the sum of the sum of the first i-th elements
+            BigInteger sumsOfsumEelemnts = 0;
+            BigInteger productOfsumElements = 1;
+            for (int i = cycleCounter; i < sum + cycleCounter; i++)
+            {
+                sumsOfsumEelemnts += list[i];
+                productOfsumElements *= list[i];
+            }
+            var bufferBuilder = new StringBuilder();
+            bufferBuilder = bufferBuilder.Append(sumsOfsumEelemnts.ToString())
+                .Append(productOfsumElements.ToString());
+
+            for (int i = sum + cycleCounter; i < list.Count; i++)
+            {
+                bufferBuilder = bufferBuilder.Append(list[i].ToString());
+            }
+            list.Clear();
+            for (int i = 0; i < bufferBuilder.Length; i++)
+            {
+                if (bufferBuilder[i] != '0' && bufferBuilder[i] != '1')
+                {
+                    list.Add(bufferBuilder[i]-'0');
+                }
+            }
+            cycleCounter++;
+        }
+
+        Console.WriteLine(String.Join(" ",list));
     }
 }
 
